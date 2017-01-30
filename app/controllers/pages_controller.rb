8b2561a6a8caa27564d65a5b472c8eb5233f8771
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-01-30 13:58:33 -0500

    final commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,36 @@
+class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
+  def welcome
+      @header = "This is the welcome page"
+  end
+
+  def about
+  end
+
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
+  end
+
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+  def secrets
+    if params[:magic_word] == "joel"
+    else
+
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'pages#welcome'
+
+  get '/welcome' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
+
+  get '/contest' => 'pages#contest'
+
+  get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+
+  get '/secrets/:magic_word' => 'pages#secrets'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
